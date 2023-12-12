133bad02ba3a4ac134ae01ecd0afb96a93c6932a
Jordan Plant <jordanplant94@gmail.com>
2023-12-12 16:07:09 +0000

    Hygraph Link added

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,28 @@ import PageOne from "./Components/Pages/Page1";
 import PageTwo from "./Components/Pages/Page2";
 import Contact from "./Components/Pages/Contact";
 import Notfound from "./Components/Pages/Notfound";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://eu-west-2.cdn.hygraph.com/content/clq29w7in57pe01uq3rc71su3/master", // Replace with your Hygraph endpoint
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="page-one" element={<PageOne />} />
-          <Route path="page-two" element={<PageTwo />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<Notfound />} />
-        </Route>
-      </Routes>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="page-one" element={<PageOne />} />
+            <Route path="page-two" element={<PageTwo />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<Notfound />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,46 +1,53 @@
 import React from "react";
 import CardItem from "./CardItem";
 import "./Cards.css";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_CARD_ITEMS = gql`
+  query GetCardItems {
+    cardItems {
+      id
+      title
+      path
+      label
+      text
+      source {
+        url
+      }
+    }
+  }
+`;
 
 function Cards() {
+  const { loading, error, data } = useQuery(GET_CARD_ITEMS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    console.error("GraphQL Error:", error);
+    if (error.networkError) console.log("Network error:", error.networkError);
+    if (error.graphQLErrors) {
+      error.graphQLErrors.forEach((err) => console.log("GraphQL error:", err));
+    }
+    return <p>Error: {error.message}</p>;
+  }
+
   return (
     <div className="cards">
-      <h1>check out these epic destinations!</h1>
+      <h1>Projects</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            <CardItem
-              src="images/img-9.jpg"
-              text="Explore the hidden waterfall deep inside the Amazon Rainforest"
-              label="adventure"
-              path="/services"
-            />
-            <CardItem
-              src="images/img-2.jpg"
-              text="Travel through the islands of Bali on a private cruise"
-              label="Luxury"
-              path="/services"
-            />
-          </ul>
-          <ul className="cards__items">
-            <CardItem
-              src="images/img-8.jpg"
-              text="Explore the hidden waterfall deep inside the Amazon Rainforest"
-              label="Adventure"
-              path="/services"
-            />
-            <CardItem
-              src="images/img-1.jpg"
-              text="Travel through the islands of Bali on a private cruise"
-              label="Adventure"
-              path="/services"
-            />
-            <CardItem
-              src="images/img-6.jpg"
-              text="Get Lost in the urban jungle of Tokyo"
-              label="City"
-              path="/services"
-            />
+            {data &&
+              data.cardItems.map((item) => (
+                <CardItem
+                  key={item.id}
+                  title={item.title}
+                  src={item.source.url} // Adjust this based on your data
+                  text={item.text}
+                  label={item.label}
+                  path={item.path}
+                />
+              ))}
           </ul>
         </div>
       </div>
