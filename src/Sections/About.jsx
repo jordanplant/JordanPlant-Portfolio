1031a8a051876de609caed7a50fe8bdb664131b7
Jordan Plant <jordanplant94@gmail.com>
2024-02-06 13:46:31 -0300

    bio update

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -12,16 +12,16 @@ const About = () => {
         <div className={styles.about__content}>
           <div className={styles.about__bio__text}>
             <p>
-              After a decade in hospitality managing cinemas across London, I
-              took the plunge into a rigorous software engineering bootcamp,
-              specialising in JavaScript, React and Node. This unique blend of
-              experiences has armed me with a deep understanding of user
-              experience and digital creativity. My degree in Film and TV
-              Production isn't just a testament to my love for visual
-              storytelling; it's the backbone of my approach to developing
-              user-centric software solutions. As I navigate the tech world, I'm
-              excited to fuse my artistic insights with cutting-edge technical
-              skills, aiming to craft web experiences that captivate and engage.
+              I'm a full-stack developer with a creative flair, prepared to
+              infuse my creative skills and managerial experience into the realm
+              of web design.
+            </p>
+            <p>
+              After completing an intensive bootcamp, I'm armed with the
+              knowledge of cutting-edge technologies like React, JavaScript, and
+              Node.js. These tools empower me to craft immersive digital
+              experiences that not only captivate users but also deliver
+              exceptional functionality.
             </p>
             <h4>Check out my projects below!</h4>
           </div>
