94ba53539d6399bf23d02b1f0dc70698395d9ab1
Jordan Plant <jordanplant94@gmail.com>
2024-02-06 12:22:19 -0300

    link update

--- src/Components/CardItem.jsx
+++ src/Components/CardItem.jsx
@@ -4,38 +4,39 @@ import styles from "../Sections/Projects.module.css";
 function CardItem(props) {
   return (
     <li className={`${styles["cards__item"]} halo-effect`}>
-      <div
-        className={styles["cards__item__pic-wrap"]}
-        data-category={props.label}
+      <a
+        className={styles["cards__item__link"]}
+        href={props.path}
+        target="_blank"
+        rel="noopener noreferrer"
       >
-        <img
-          src={props.src}
-          alt={props.title}
-          className={styles["cards__item__img"]}
-        />
-      </div>
-      <div className={styles["cards__item__info"]}>
-        <a
-          className={styles["cards__item__link"]}
-          href={props.path}
-          // target="_blank"
-          // rel="noopener noreferrer"
+        <div
+          className={styles["cards__item__pic-wrap"]}
+          data-category={props.label}
         >
+          <img
+            src={props.src}
+            alt={props.title}
+            className={styles["cards__item__img"]}
+          />
+        </div>
+        <div className={styles["cards__item__info"]}>
           <h3 className={styles["cards__item__title"]}>{props.title}</h3>
-        </a>
-        <p className={styles["cards__item__text"]}>{props.text}</p>
-        {props.githubLink && (
-          <a
-            className={styles["card__github"]}
-            href={props.githubLink}
-            // target="_blank"
-            // rel="noopener noreferrer"
-            aria-label="Github"
-          >
-            <i className="fa-brands fa-github fa-2xl"></i>
-          </a>
-        )}
-      </div>
+
+          <p className={styles["cards__item__text"]}>{props.text}</p>
+          {props.githubLink && (
+            <a
+              className={styles["card__github"]}
+              href={props.githubLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Github"
+            >
+              <i className="fa-brands fa-github fa-2xl"></i>
+            </a>
+          )}
+        </div>
+      </a>
     </li>
   );
 }
