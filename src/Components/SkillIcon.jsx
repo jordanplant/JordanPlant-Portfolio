de1e3ff17d4f0695165c8e5e435d623fb6b793ec
Jordan Plant <jordanplant94@gmail.com>
2024-02-12 21:08:51 -0300

    remove hover colours

--- src/Components/SkillIcon.jsx
+++ src/Components/SkillIcon.jsx
@@ -1,17 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "../Sections/Skills.module.css";
 
 const SkillIcon = ({ skill }) => {
-  const [hover, setHover] = useState(false); // State to manage hover
-
   const iconColor = skill.color && skill.color.hex ? skill.color.hex : "#000";
 
   return (
     <div
-      className={`${styles["skill__icon"]} ${hover ? styles["hover"] : ""}`}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-      style={{ "--hover-shadow-color": iconColor }} // Using iconColor for the shadow
+      className={styles["skill__icon"]}
+      style={{ "--hover-shadow-color": iconColor }} // currently iconcolor is turned off
     >
       <img src={skill.icon.url} alt={skill.skill} />
       {/* Additional details can be added here */}
